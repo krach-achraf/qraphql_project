e9257a69f656b34226b624ef72302b9b621ba9c9
unknown <krachachraf98@gmail.com>
2022-08-09 18:04:30 +0100

    cors policy

--- src/main/java/com/example/graphqltraining/GraphQlTrainingApplication.java
+++ src/main/java/com/example/graphqltraining/GraphQlTrainingApplication.java
@@ -2,6 +2,15 @@ package com.example.graphqltraining;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
+
 @SpringBootApplication
 public class GraphQlTrainingApplication {
 
@@ -9,4 +18,17 @@ public class GraphQlTrainingApplication {
         SpringApplication.run(GraphQlTrainingApplication.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("http://127.0.0.1:5173/"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 }
